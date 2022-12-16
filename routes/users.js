a81b978abb82fb0a6a9df65c2f57430ea1cac728
LontchiFrank <fodjof6@gmail.com>
2022-12-16 11:14:35 +0100

    so different

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: "23",
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: "20",
-  },
-];
+let users = [];
 
 router.get("/", (req, res) => {
   console.log("Hello from Homepage");
@@ -33,6 +22,46 @@ router.post("/", (req, res) => {
   users.push({ ...user, id: uuidv4() });
 
   res.send(`User with the name ${user.firstName}  added to the database `);
+});
+
+// /users/2 => req.params
+
+router.get("/:id", (req, res) => {
+  //   console.log(req.params);
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with the id ${id} deleted from the database`);
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+
+  const user = users.find((user) => user.id === id);
+
+  if (firstName) {
+    user.firstName = firstName;
+  }
+
+  if (lastName) {
+    user.lastName = lastName;
+  }
+
+  if (age) {
+    user.age = age;
+  }
+
+  res.send(`User with id ${id} has been changed`);
 });
 
 export default router;
